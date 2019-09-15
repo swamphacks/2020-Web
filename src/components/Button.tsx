468924da7981ab627be8cb7f859b93d83e586a2d
siddhatiwari@gmail.com <siddhatiwari@gmail.com>
2019-09-15 16:49:04 -0400

    Button fix

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -4,29 +4,49 @@ import styled from 'styled-components';
 import buttonImage from '../assets/buttonImage.svg';
 
 const CustomButton = styled.button`
-  z-index: 99;
-  width: 200px;
-  height: 50px;
-  border: 0;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  background: transparent;
-  color: white;
-  font-family: system-ui, sans-serif;
-  font-size: 1rem;
-  line-height: 1.2;
-  white-space: nowrap;
-  text-decoration: none;
-  padding: 0.25rem 0.5rem;
-  margin: 0.25rem;
-  cursor: pointer;
+z-index: 99;
+width: 200px;
+height: 50px;
+border: 0;
+display: -webkit-box;
+display: -ms-flexbox;
+display: flex;
+-webkit-box-align: center;
+-ms-flex-align: center;
+        align-items: center;
+-webkit-box-pack: center;
+-ms-flex-pack: center;
+        justify-content: center;
+background: transparent;
+color: white;
+font-family: system-ui, sans-serif;
+font-size: 1rem;
+line-height: 1.2;
+white-space: nowrap;
+text-decoration: none;
+padding: 0.25rem 0.5rem;
+margin: 0.25rem;
+cursor: pointer;
 
 
-  transition: transform .25s ease;
-  :hover {
-    transform: scale(1.09) rotate(${(Math.random() * 3) - 1}deg);
-  }  
+-webkit-transition: -webkit-transform .25s ease;
+
+
+transition: -webkit-transform .25s ease;
+
+
+-o-transition: transform .25s ease;
+
+
+transition: transform .25s ease;
+
+
+transition: transform .25s ease, -webkit-transform .25s ease;
+:hover {
+  -webkit-transform: scale(1.09) rotate(${(Math.random() * 3) - 1}deg);
+      -ms-transform: scale(1.09) rotate(${(Math.random() * 3) - 1}deg);
+          transform: scale(1.09) rotate(${(Math.random() * 3) - 1}deg);
+}  
 
 `;
 
